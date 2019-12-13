49399e5fcb7254e390f35140da387f3b1f619eab
David Nyman <davenyman@gmail.com>
2019-12-13 09:34:38 -0500

    Updates

---
+++ precache-manifest.e389b80c64fb72709a465cd530d0363b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74349cf83449dcce76e75fab8bbd518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82071b384e729eaab687",
+    "url": "/static/css/main.f0877a64.chunk.css"
+  },
+  {
+    "revision": "aabae6855b0147d25685",
+    "url": "/static/js/2.0657dd8b.chunk.js"
+  },
+  {
+    "revision": "82071b384e729eaab687",
+    "url": "/static/js/main.35bac942.chunk.js"
+  },
+  {
+    "revision": "06025003f49d7f9576b0",
+    "url": "/static/js/runtime-main.be26e9c1.js"
+  }
+]);
